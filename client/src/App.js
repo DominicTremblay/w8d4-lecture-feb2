e3153c6c5a684654be6f6342243725943f11fa75
Dominic Tremblay <dominictremblay24@gmail.com>
2020-03-25 21:39:46 -0400

    added axios request

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const state = {
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: '/quotes'
+    })
+      .then(response => {
+        this.setState({ quotes: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return <div className="App"></div>;
   }
