a074d057909541f86fdd74bb3c51c5f7b3f4310d
Dominic Tremblay <dominictremblay24@gmail.com>
2020-03-25 22:38:33 -0400

    created AddQuote

---
+++ client/src/AddQuote.js
@@ -0,0 +1,49 @@
+import React, { useState, Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class AddQuote extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      redirect: false
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>Add a New Quote</h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <div style={{ width: '20em' }}>
+              <div className="form-group">
+                <input
+                  style={{ width: '100%' }}
+                  type="text"
+                  name="quote"
+                  id="quote"
+                  placeholder="Please enter your quote"
+                  value={this.state.text}
+                  onChange={event =>
+                    this.setState({ text: event.target.value })
+                  }
+                />
+              </div>
+
+              <input type="submit" className="btn btn-primary" value="Add" />
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+export default AddQuote;
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Switch, Route, Link } from 'react-router-dom';
+import AddQuote from './AddQuote';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    const state = {
+    this.state = {
       quotes: []
     };
   }
@@ -22,7 +24,44 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App"></div>;
+    return (
+      <>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <Link to="/quotes/new" className="nav-link">
+                  Add New Quote
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route path="/quotes/new" component={AddQuote} />
+        </Switch>
+      </>
+    );
   }
 }
 
